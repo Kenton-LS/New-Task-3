9514dbf49fc1436e94536ad8555dbdfc3dc507a6
Kenton <kentonlesliesmith@gmail.com>
2019-10-23 09:39:12 +0200

    Building
    Updated building class. Removed spaghetti.

--- MODEL CODE/MODEL CODE/Building.cs
+++ MODEL CODE/MODEL CODE/Building.cs
@@ -6,17 +6,11 @@ using System.Threading.Tasks;
 
 namespace MODEL_CODE
 {
-    abstract class Building
+    abstract class Building : Target //buildings are targets
     {
-        protected int x, y, health, maxHealth;
-        protected string faction;
         protected char symbol;
-        protected bool isDestroyed = false;
 
-
-        public Building(int x, int y, int health, /*int maxHealth, */char symbol, string faction/*, 
-                        string resourceType, int resourcesGenerated, int resourcePoolRemaining, int resourcesPerRound,
-                        string factoryUnitType, int productionSpeed, int spawnPoint*/)
+        public Building(int x, int y, int health, char symbol, string faction)
         {
             this.x = x; 
             this.y = y;
@@ -29,22 +23,9 @@ namespace MODEL_CODE
 
         public Building() { }
 
-        public int X { get { return x; } } //other classess are supposed to change the variable, ONLY THEN do you add a 'set'for exposing.
-
-        public int Y { get { return y; } } //removed the 'abstract' properties
-
-        public int Health { get { return health; } set { health = value; } }
-
         public char Symbol { get { return symbol; } }
 
-        public string Faction { get { return faction; } }
-
-       // public abstract void Destroy(); //moved up here, removed bool
-
         public abstract string SaveGame(); //replaced the save void instead of abstract ToString(), the method is already virtual
-
-        public bool IsDestroyed { get { return isDestroyed; } }
-        ///
 
         public override string ToString() //the properties on both the base and inherited classes
         {
@@ -55,7 +36,8 @@ namespace MODEL_CODE
                    //"RSS PER ROUND:  " + resourcesPerRound + "\n";
         }
 
-        public virtual void Destroy() //death method
+
+        public override void Destroy() //death method
         {
             isDestroyed = true;
             symbol = 'X';
