d5e06c717e795c5fa95f3bf8069f64c4887fbc3e
Kenton <kentonlesliesmith@gmail.com>
2019-10-23 08:44:43 +0200

    RangedUnit
    Reformatting

--- MODEL CODE/MODEL CODE/RangedUnit.cs
+++ MODEL CODE/MODEL CODE/RangedUnit.cs
@@ -9,17 +9,16 @@ namespace MODEL_CODE
 {
     class RangedUnit : Unit
     {
-        public RangedUnit(int x, int y, /*int health, int maxHealth, int speed, int attack, int attackRange, char symbol,*/ string faction/*, string nameUnit*/)
-              : base(x, y, 5, 5, 1, 2, 3, '?', faction, "Ranged") { } //use the base keyword
+        public RangedUnit(int x, int y, string faction) : base(x, y, 5, 5, 1, 2, 3, '?', faction, "Ranged") { } //use the base keyword
 
         public RangedUnit(string values) : base(values) { } //shortened due to unit class having these fused
-
 
         public override string SaveGame()
         {
             return string.Format(
-                $"Ranged, {x}, {y}, {health}, {maxHealth}, {speed}, {attack}, {attackRange}, " + $"{faction}, {symbol}, {nameUnit}, {IsDestroyed}");
+                $"Ranged, {x}, {y}, {health}, {maxHealth}, {speed}, {attack}, {attackRange}, " + 
+                $"{faction}, {symbol}, {nameUnit}, {IsDestroyed}");
         }
-    
     }
 }
+//reformatting
